c92c5555b9b959157b81619bdbf67308b651eb7a
Chris Hartman <chris.m.hartman@gmail.com>
2020-03-30 14:12:38 -0800

    Add Observer and Subject design pattern

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@ using std::endl;
 #include "Expression.hpp"
 using std::make_unique;
 using std::unique_ptr;
+
 #include "StoreByReference.hpp"
 #include "StoreByPointer.hpp"
 
 #include "moderncppexamples.hpp"
+
+#include "Observer.hpp"
+using std::make_shared;
+#include "Subject.hpp"
 
 void demonstrateExpressions() {
   unique_ptr<Expression> fifteen = make_unique<NumberExpression>(15);
@@ -25,8 +30,15 @@ void demonstrateExpressions() {
       move(s), make_unique<MinusExpression>(make_unique<NumberExpression>(13),
                                             make_unique<NumberExpression>(9)));
 
-  cout << "The value of " << comp->toString() << " is " << comp->evaluate()
-       << std::endl;
+   cout << "The value of " << comp->toString() << " is " << comp->evaluate()
+        << std::endl;
+
+   auto compClone = comp->clone();
+
+   auto squareIt = make_unique<DivideExpression>(move(comp),move(compClone));
+
+   cout << "The value of " << squareIt->toString() << " is " << squareIt->evaluate()
+        << std::endl;
 }
 
 
@@ -57,10 +69,30 @@ void demonstrateStoreByPointer() {
     kim.repair();
 }
 
+void demonstrateObserver() {
+   Subject s;
+
+   auto co = make_shared<CoutObserver>();
+   auto co2 = make_shared<CoutObserver>();
+   auto bo = make_shared<BarGraphObserver>();
+   s.attach(co);
+   s.attach(co);
+   s.attach(co2);
+   s.attach(bo);
+
+   s.setN(13);
+   s.detach(co2);
+   s.setN(67);
+   s.detach(co);
+   s.setN(1);
+}
+
 int main() {
-    demonstrateStoreByReference();
-    demonstrateStoreByPointer();
-  return 0;
+//    demonstrateStoreByReference();
+//    demonstrateStoreByPointer();
+//   demonstrateExpressions();
+   demonstrateObserver();
+   return 0;
 }
 
 // Possible extensions:
